1f983da6020e7337d22dc3391d3726ed5928b609
Michael Tuzhilov <michael.tuzhilov@firemon.com>
2015-10-01 16:15:38 -0500

    Refactoring...

--- Parser.java
+++ Parser.java
@@ -2,41 +2,80 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 /**
  * This class is thread safe.
  */
+
 public class Parser {
+
+  private Parser() {}
+
+  private static final Parser INSTANCE = new Parser();
+
+
+  public static Parser getInstance() {
+    return INSTANCE;
+  }
+
   private File file;
+
   public synchronized void setFile(File f) {
     file = f;
   }
-  public synchronized File getFile() {
+
+  public File getFile() {
     return file;
   }
+
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+    FileInputStream i = null;
+    try {
+      i = new FileInputStream(file);
+      String output = "";
+      int data;
+      while ((data = i.read()) > 0) {
         output += (char) data;
       }
+      return output;
+    } finally {
+      if (i != null) {
+        i.close();
+      }
     }
-    return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+
+  public String getContentWithoutUnicode() throws IOException {
+    FileInputStream i = null;
+    try {
+      i = new FileInputStream(file);
+      String output = "";
+      int data;
+      while ((data = i.read()) > 0) {
+        if (data > 0x80) {
+          output += (char) data;
+        }
+      }
+      return output;
+    } finally {
+      if (i != null) {
+        i.close();
+      }
+    }
+  }
+
+  public synchronized void saveContent(String content) throws IOException {
+    FileOutputStream o = null;
+    try {
+      o = new FileOutputStream(file);
+      for (int i = 0; i < content.length(); i += 1) {
+        o.write(content.charAt(i));
+      }
+    } finally {
+      if (o != null) {
+        o.close();
+      }
     }
   }
 }
+
